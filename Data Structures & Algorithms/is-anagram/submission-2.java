024b3281611410a819db51619e4577fda23f765d
Binore Mohapatra <binoremohapatra@gmail.com>
2026-08-08 11:58:06 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+         if(s.length() != t.length() ){
+            return false ;
+
+         }
+         char[] sl = s.toCharArray();
+         char[] tl = t.toCharArray();
+         Arrays.sort(sl);
+         Arrays.sort(tl);
+
+
+          return Arrays.equals(sl,tl);
+    }
+}
